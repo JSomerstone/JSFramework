89d83cf2527b6e40ba7014efdfc9a1150f2c5fe9
Joona Somerkivi <joona.somerkivi@solinor.com>
2012-07-07 01:44:54 +0300

    Improved documentation, added test cases

--- Core/Request.php
+++ Core/Request.php
@@ -3,22 +3,45 @@ namespace keijoCMS\Core;
 
 class Request
 {
+    /**
+     * Placeholder for both POST and GET params
+     * @var array
+     */
     protected $requestParams = array();
+
+    /**
+     * Placeholder for POST params
+     * @var array
+     */
     protected $postParams = array();
+
+    /**
+     * Placeholder for GET params
+     * @var array
+     */
     protected $getParams = array();
 
+    /**
+     * Will initialize Request object and clear possible $_GET and $_POST parameters
+     */
     public function __construct()
     {
         $this->postParams = $_POST;
         $this->getParams = $_GET;
 
         $this->requestParams = array_merge($this->getParams, $this->postParams);
+
         $_GET = array();
         $_POST = array();
     }
 
-
-    public function post($param = null)
+    /**
+     * Get POST param with or without index
+     * if $param not given, will return all POST-params as an array
+     * @param string $param POST-parameter to return, optional
+     * @return string
+     */
+    public function getPost($param = null)
     {
         if (null === $param)
         {
@@ -27,6 +50,54 @@ class Request
         else if (isset($this->postParams[$param]))
         {
             return $this->postParams[$param];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Get GET param with or without index
+     * if $param not given, will return all GET-params as an array
+     * @param string $param GET-parameter to return, optional
+     * @return string
+     */
+    public function getGet($param = null)
+    {
+        if (null === $param)
+        {
+            return $this->getParams;
+        }
+        else if (isset($this->getParams[$param]))
+        {
+            return $this->getParams[$param];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Get POST or GET param with or without index
+     * if $param not given, will return all GET & POST-params as single array
+     * @param string $param request parameter to return, optional
+     * @return string
+     */
+    public function getRequest($param = null)
+    {
+        if (null === $param)
+        {
+            return $this->requestParams;
+        }
+        else if (isset($this->requestParams[$param]))
+        {
+            return $this->requestParams[$param];
+        }
+        else
+        {
+            return null;
         }
     }
 }
